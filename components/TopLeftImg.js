94b22c996e298cc3bb7de14ae1d4830a96979cd2
Lucas <lucasfsouza.l10@gmail.com>
2023-10-13 18:59:40 -0300

    updating images

--- components/TopLeftImg.js
+++ components/TopLeftImg.js
@@ -1,10 +1,32 @@
 // next image
 import Image from "next/image";
+
+// Providers themes
+import { useTheme, ThemeProvider } from "next-themes";
+import { useEffect, useState } from "react";
+
+
 const TopLeftImg = () => {
+  const { theme, setTheme } = useTheme();
+  const [pathImage, setPathImage] = useState('');
+
+  const currentTheme = theme === 'system' ? systemTheme : theme;
+
+
+  useEffect(()=>{
+    console.log("Theme: ", currentTheme);
+    setPathImage(currentTheme === 'light' ? '/top-left-img-light.png':'/top-left-img-dark.png')
+    
+  },[currentTheme])
+
+
   return (
-    <div className="absolute left-0 top-0 mix-blend-color-dodge z-10 w-[200px] xl:w-[400px] opacity-50">
-      <Image src="/top-left-img.png" width={400} height={400} alt="" />
-    </div>
+      <div className="absolute left-0 top-0 z-10 w-[200px] xl:w-[400px]">
+
+        <Image src={pathImage} width={400} height={400} alt="" />
+
+        
+      </div>
   );
 };
 
